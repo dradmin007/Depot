79c672c8e513d8847f7de02ffac711bcef0ebca9
DrAdmin <stas@localhost.localdomain>
2014-06-19 18:19:50 +0400

    Add Extended Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: {minimum: 10}
@@ -8,4 +11,13 @@ class Product < ActiveRecord::Base
      message: 'must be a URL for GIF, JPG, PNG image.'
      # URL должен указывать на изображение формата GIF, JPG или PNG
     }
+  private
+  # убедаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base,'существуют товарные позиции')
+    end
+  end
 end
