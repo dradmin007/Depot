8548adea7aecf9b7d9fc4bf7b1e7c03c878ed528
DrAdmin <stas@localhost.localdomain>
2014-06-20 11:02:19 +0400

    End of Chapter 11

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -69,7 +69,7 @@ class CartsController < ApplicationController
     @cart.destroy
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_url, notice: 'Теперь Ваша корзина пуста!' }
+      format.html { redirect_to store_url }
       format.json { head :ok }
     end
   end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,9 +31,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
-        #format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
+        format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
