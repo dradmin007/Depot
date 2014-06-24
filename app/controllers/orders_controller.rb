9ad084a56703129cb72df5e6250598f406f5ff6c
DrAdmin <stas@localhost.localdomain>
2014-06-24 15:45:15 +0400

    Chapter 13.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,12 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.order('created_at').paginate page: params[:page], per_page: 10
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @orders}
+    end
   end
 
   # GET /orders/1
@@ -37,7 +42,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-
+        OrderNotifier.received(@order).deliver  
         format.html { redirect_to store_url, notice: 'Благодарим за Ваш заказ.' }
         format.json { render json: @order, status: :created, location: @order }
       else
