b82ae9e75fa4f0410081e6b20d46da969ce15e1c
DrAdmin <stas@localhost.localdomain>
2014-06-27 07:21:17 +0400

    Chapter 14. Ok!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def authorize
-    logger.error "Authorize(0): #{session[:user_id]}"
     unless User.find_by_id(session[:user_id])
       redirect_to login_url, notice: "Пройдите авторизацию"
     end
