a7ecd62a7864272e2313f5d2de950156a49ac142
DrAdmin <stas@localhost.localdomain>
2014-06-30 16:37:36 +0400

    Chapter 14. Locales

--- config/routes.rb
+++ config/routes.rb
@@ -7,22 +7,25 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-
-  get 'store/index'
-
-  resources :products do
-    get :who_bought, on: :member
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
+    root to: 'store#index', as: 'store'
   end
+
+  #get 'store/index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
+  #root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
